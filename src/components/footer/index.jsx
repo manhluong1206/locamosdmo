de007b0993c3f19572374a4a1655c31d42595b84
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-27 12:46:24 +0700

    update footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,14 +4,16 @@ import './index.scss'
 export default function Footer() {
     return (
         <section id='footer' className='footer'>
-            <div className='container text-end'>
-                <div class="col-sm-12">
+            <div className='container'>
+                <div className='row pl-4 pr-4'>
+                    <div class="col-sm-12">
                     <div class="search-container">
                         <form action="/action_page.php">
                             <button type="submit"><i class="fa fa-search"></i></button>
                             <input type="text" name="search" />
                         </form>
                     </div>
+                </div> 
                 </div>
             </div>
         </section>
