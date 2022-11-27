08b03dd6fd8f5e43be82d7bed767378972446f68
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-27 17:54:57 +0700

    update

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,12 +6,8 @@ export default function Footer() {
         <section id='footer' className='footer'>
             <div className='container'>
                 <div className='row pl-4 pr-4'>
-                    <div class="box">
-                        <div class="container-1">
-                            <span class="icon text-left"><i class="fa fa-search"></i></span>
-                            <input type="search" id="search"/>
-                        </div>
-                    </div>
+                    <span class="icon position-absolute"><i class="fa fa-search"></i></span>
+                    <input type="search" id="search" />
                 </div>
             </div>
         </section>
