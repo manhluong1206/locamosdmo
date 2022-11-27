314f13b66d5938abfbd76051a7c83bb3ff19888c
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-27 13:07:22 +0700

    update test footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,14 +6,20 @@ export default function Footer() {
         <section id='footer' className='footer'>
             <div className='container'>
                 <div className='row pl-4 pr-4'>
-                    <div class="col-sm-12">
-                    <div class="search-container">
-                        <form action="/action_page.php">
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                            <input type="text" name="search" />
-                        </form>
+                    <div class="box">
+                        <div class="container-1">
+                            <span class="icon"><i class="fa fa-search"></i></span>
+                            <input type="search" id="search" placeholder="Search..." />
+                        </div>
                     </div>
-                </div> 
+                    {/* <div class="col-sm-12">
+                        <div class="search-container">
+                            <form action="/action_page.php">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                                <input type="text" name="search" />
+                            </form>
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </section>
