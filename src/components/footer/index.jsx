dca82a629e83fb3ea9729fd2a5b90e2b9424d638
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-27 17:48:13 +0700

    update the footer for the second time

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,8 +7,8 @@ export default function Footer() {
             <div className='container'>
                 <div className='row pl-4 pr-4'>
                     <div class="box">
-                        <div class="container-1">
-                            <span class="icon text-left"><i class="fa fa-search"></i></span>
+                        <div class="container-1 text-end ">
+                            <span class="icon"><i class="fa fa-search"></i></span>
                             <input type="search" id="search"/>
                         </div>
                     </div>
