1216b5e2d62dd59a7daf4081ba84181a1812be17
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-27 18:07:20 +0700

    update distance

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,9 +3,9 @@ import './index.scss'
 
 export default function Footer() {
     return (
-        <section id='footer' className='footer p-2'>
+        <section id='footer' className='footer mt-2 pt-2 pb-2'>
             <div className='container'>
-                <div className='row pl-4 pr-4 text-left'>
+                <div className='row text-left'>
                     <span class="icon position-absolute"><i class="fa fa-search"></i></span>
                     <input type="search" id="search" />
                 </div>
